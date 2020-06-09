de9c7d8fd8588f516ca73dcacf78a67c6a9fb3e6
yeonhodev <yeonho.dev@gmail.com>
2020-06-08 23:22:44 -0400

    basic syntax

---
+++ lecture07.py
@@ -0,0 +1,38 @@
+# Print a single item
+print("# Print one")
+print("Hello python programming...!")
+print()
+
+# Print multiple itmes
+print("#Print multiple items")
+print(10, 20, 30, 40 ,50)
+print("Hi, ", "my", "name", "is", "Daniel.")
+print()
+
+# Print nothing
+print("check")
+print()
+print()
+print("check")
+
+# Escape
+print('He said, "Hi!"')
+print("He thinks, 'yo'")
+print("He said, \"Hi!\"")
+print("Hi\nHi")
+print("Yo\tYo")
+print("\\")
+print("""
+One
+Two
+Three
+""")
+print("-----")
+print("""\
+Four
+Five
+Six\
+""")
+print("Hi" * 3)
+print(3 * "Hi")
+print("-" * 20)
