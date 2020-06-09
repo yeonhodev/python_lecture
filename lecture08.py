36d6297da66f29226064c53f5ae00f2694fca975
yeonhodev <yeonho.dev@gmail.com>
2020-06-08 23:32:28 -0400

    basic syntax

---
+++ lecture08.py
@@ -0,0 +1,13 @@
+print("World"[0])
+print("World"[1])
+print("World"[2])
+print("World"[3])
+print("World"[4])
+
+print("World"[0:1]) #파이썬은 0 <= X < 1 으로 해석 -> 1을 포함하지 않음
+print("World"[0:2]) 
+print("World"[0:3]) 
+print("World"[3:]) #ld 
+print("World"[:2]) #Wo 
+
+print(len("World"))
