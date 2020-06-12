742199246f24e2d806524c14692ca9c050af3d20
yeonhodev <yeonho.dev@gmail.com>
2020-06-11 23:59:39 -0400

    Example question

---
+++ lecture15.py
@@ -0,0 +1,8 @@
+a = int(input("> First number: "))
+b = int(input("> Second number: "))
+print()
+
+if a > b:
+    print("The first number, {}, is bigger than {}".format(a, b))
+if a < b:
+    print("The second number, {}, is bigger than {}.".format(b, a))
