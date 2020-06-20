89eb60710254095fa1f415d4d047c57ccc80e47e
yeonhodev <yeonho.dev@gmail.com>
2020-06-19 23:57:25 -0400

    dictionary add/delete element

--- lecture23.py
+++ lecture23.py
@@ -4,9 +4,27 @@ list[0]
 
 dictionary = {
     "keyA": "valueA",
-    "keyB": "valueB",
-    "keyC": "valueC"
+    273: [1, 2, 3, 4],
+    True: False
 }
 print(dictionary["keyA"])
-print(dictionary["keyB"])
-print(dictionary["keyC"])
+print(dictionary[273])
+print(dictionary[True])
+
+dictionary["keyA"] = "valuevalue"
+print(dictionary["keyA"])
+
+for key in dictionary:
+    print("{} : {}".format(key, dictionary[key]))
+
+# 새로운 키와 값 추가
+dictionary["newkey"] = "newvalue"
+
+for key in dictionary:
+    print("{} : {}".format(key, dictionary[key]))
+
+# 값 제거하기
+del dictionary["newkey"]
+print()
+for key in dictionary:
+    print("{} : {}".format(key, dictionary[key]))
