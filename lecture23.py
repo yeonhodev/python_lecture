eecd467f293a8ab086c702ef03a5b4c8eeb83a37
yeonhodev <yeonho.dev@gmail.com>
2020-06-19 23:51:24 -0400

    Added lecture23.py

---
+++ lecture23.py
@@ -0,0 +1,12 @@
+list = []
+list = [1, 2, 3, 4, 5]
+list[0]
+
+dictionary = {
+    "keyA": "valueA",
+    "keyB": "valueB",
+    "keyC": "valueC"
+}
+print(dictionary["keyA"])
+print(dictionary["keyB"])
+print(dictionary["keyC"])
