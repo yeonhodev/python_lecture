5108a1a104daca11a54e6f259ccb6e193b2717f3
yeonhodev <yeonho.dev@gmail.com>
2020-06-26 23:48:15 -0400

    Added lecture27.py

---
+++ lecture27.py
@@ -0,0 +1,29 @@
+i = 0
+
+while True:
+    print("{}번째 반복문입니다.".format(i))
+    i += 1
+
+    input_text = input("> 종료하시겠습니까?(y)")
+    if input_text in ["Y", "y"]:
+        print("반복을 종료합니다.")
+        break
+
+numbers = [5, 15, 6, 20, 7, 25]
+
+for number in numbers:
+    if number < 10:
+        continue #현재 반복을 중지하고, 다음 반복으로 넘어간다. 
+    print(number)
+
+# Continue 사용 안하고 같은 결과가 가능 1
+for number in numbers:
+    if number >= 10:
+        print(number)
+
+# Continue 사용 안하고 같은 결과가 가능 2
+for number in numbers:
+    if number < 10:
+        pass
+    else:
+        print(number)
