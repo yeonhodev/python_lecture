3fdbb0c3c12e83c1c83174a380c51abe3a2cdb2e
yeonhodev <yeonho.dev@gmail.com>
2020-07-08 00:24:22 -0400

    Reason to use functions 1

--- lecture36.py
+++ lecture36.py
@@ -13,3 +13,21 @@ def flatten(data):
 example = [[1, 2, 3], [4, [5, 6]], 7, [8, 9]]
 print("원본:", example)
 print("변환:", flatten(example))
+
+#주석을 잘 다는 것이 코드를 잘 작성하는 것이다. 하지만, 함수를 잘 사용하면, 주석 조차도 필요 없을 만큼 가독성이 좋아진다. -> 훌륭한 예는 아래와 같다.
+
+#함수 정의
+# 상수는 아래의 PI와 같이 변수로 따로 빼서 설정해 주는 것이 좋다. 
+PI = 3.14
+def number_input():
+    output = input("숫자 입력> ")
+    return float(output)
+def get_circumference(radius):
+    return 2 * PI * radius
+def get_circle_area(radius):
+    return PI * radius * radius
+
+#코드 본문
+radius = number_input()
+print(get_circumference(radius))
+print(get_circle_area(radius))
