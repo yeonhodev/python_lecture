198b7e04bf38e6976437816c0205ba22f68640e6
yeonhodev <yeonho.dev@gmail.com>
2020-07-18 23:49:33 -0400

    except ValueError, IndexError, Exception with if

--- lecture44.py
+++ lecture44.py
@@ -17,7 +17,34 @@ try:
 #except 예외의종류 as 변수로사용할이름:
 #예외의종류에 무엇을 넣을지 모를 때는 모든 예외들의 어머니로 부를 수 있는 Exception을 넣는다 -> Exception 처럼 대문자로 시작하는 것은 Class임
 except Exception as exception:
-  print(type(exception))
-  print(exception)
+  #print(type(exception))
+  #print(exception)
+  if type(exception) == ValueError:
+    print("값에 문제가 있습니다.")
 
-# 2:30
+
+# if 조건문으로 예외 구분하기
+try:
+  a = [273, 103, 52, 57, 100]
+  number = int(input("정수 입력(0~4까지 입력 해 주세요.)> "))
+  print(a[number])
+except Exception as exception:
+  print(type(exception))
+  if type(exception) == ValueError:
+    print("값에 문제가 있습니다.")
+  elif type(exception) == IndexError:
+    print("0~4까지 입력 해 주세요.")
+
+#들여쓰기를 사용하지 않고 윗쪽의 코드를 똑같이 구현하는 방법
+try:
+  a = [273, 103, 52, 57, 100]
+  number = int(input("정수 입력(0~4까지 입력 해 주세요.)> "))
+  print(a[number])
+except ValueError as exception:
+  print("값에 문제가 있습니다.")
+except IndexError as exception:
+  print("0~4까지 입력 해 주세요.")
+except Exception as exception: #마지막에는 오류가 나더라도 꼭 걸릴 수 있게 Exception을 무조건 사용해 주는 것이 좋다. 
+  print("알 수 없는 예외가 발생했습니다.")
+  #개발자에게 메일을 보낸다. 
+    
