0aaa49da89312234a86240d8b687d2f65feb9322
yeonhodev <yeonho.dev@gmail.com>
2020-07-20 23:41:04 -0400

    refactoring with functions

--- lecture45.py
+++ lecture45.py
@@ -17,4 +17,38 @@ for student in students:
   # 출력합니다.
   print(student["name"], score_sum, score_average, sep="\t")
 
-# 3:28
+print()
+
+
+
+# 위의 코드를 함수로 똑같이 구현하기 -> 행위 정하기
+def 학생_생성(name, korean, math, english, science):
+  return{
+    "name": name,
+    "korean": korean,
+    "math": math,
+    "english": english,
+    "science": science
+  }
+
+def 총점(student):
+  return student["korean"] + student["math"] + student["english"] + student["science"]
+def 평균(student):
+  return 총점(student) / 4
+def 출력(student):
+  print(student["name"], 총점(student), 평균(student), sep="\t")
+
+#---------------------------------------- #
+#학생 리스트를 선언합니다.
+students = [
+  학생_생성("윤인성", 87, 98, 88, 95),
+  학생_생성("연하진", 92, 98, 96, 98),
+  학생_생성("구지연", 76, 96, 94, 90),
+  학생_생성("나선주", 98, 92, 96, 92),
+  학생_생성("윤아린", 95, 98, 98, 98),
+  학생_생성("윤명월", 64, 88, 92, 92)
+]
+
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+  출력(student)
