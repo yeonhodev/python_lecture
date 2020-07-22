0ac1b69413054ef8605a62c3cb81f10b9079aa41
yeonhodev <yeonho.dev@gmail.com>
2020-07-21 22:39:10 -0400

    class

--- lecture45.py
+++ lecture45.py
@@ -52,3 +52,33 @@ students = [
 print("이름", "총점", "평균", sep="\t")
 for student in students:
   출력(student)
+
+print()
+
+# Class 구문 - 속성과 행위 합치기
+class 학생:
+  def __init__(self, name, korean, math, english, science):
+    self.name = name
+    self.korean = korean
+    self.math = math
+    self.english = english
+    self.science = science
+  def 총점(self):
+    return self.korean + self.math + self.english + self.science
+  def 평균(self):
+    return self.총점() / 4
+  def 출력(self):
+    print(self.name, self.총점(), self.평균(), sep="\t")
+
+students = [
+  학생("윤인성", 87, 98, 88, 95),
+  학생("연하진", 92, 98, 96, 98),
+  학생("구지연", 76, 96, 94, 90),
+  학생("나선주", 98, 92, 96, 92),
+  학생("윤아린", 95, 98, 98, 98),
+  학생("윤명월", 64, 88, 92, 92)
+]
+
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+  student.출력()
